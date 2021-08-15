63237cc14e4c8cdcf3a89ff129f9047dbcc825dd
Rad1618 <81822443+Rad1618@users.noreply.github.com>
2021-08-15 12:17:41 +0200

    Update sceneBattle.js

--- sceneBattle.js
+++ sceneBattle.js
@@ -12,12 +12,10 @@ let SceneBattle = new Phaser.Class({
         this.load.image('testCard', 'assets/testCard.png');
         this.load.image('testCardFire', 'assets/testCardFire.png');
         this.load.image('atomBomb', 'assets/atomBomb.png');
-        this.load.image('forest_Card', 'assets/forest_1.png');
     },
 
     create: function () {
-        //this.testCard = new Card(this, testCardData, 200, 200);
-        //this.forest_Card = new Card(this, testCardData, 400, 200);
+        this.testCard = new Card(this, testCardData, 200, 200);
     },
 
     update: function (timestep, dt) {
