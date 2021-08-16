dfae5f040a5e909d7f3b53a40c39444154992cb2
Rad1618 <81822443+Rad1618@users.noreply.github.com>
2021-08-16 12:03:21 +0200

    Update cardsData.js

--- data/cardsData.js
+++ data/cardsData.js
@@ -1,15 +1,53 @@
-let testCardData = {
+/*let testCardData = {
     "name": "Bomba atomowa",
     "element": "fire",
     "flavour": "Całkiem jeszcze nowa",
     "value": 17,
     "image": "atomBomb"
-};
+};*/
 
-let forest_1 = {
+let forest_1 =
+{
     "ID": 0,
     "name": "Zoledz",
     "element": "forest",
     "value": 1,
     "image": "im_forest_1"
+};
+
+let forest_13 =
+{
+    "ID": 12,
+    "name": "Apokalipsa baobabow",
+    "element": "forest",
+    "value": 13,
+    "image": "im_forest_13"
+};
+
+let fire_1 =
+{
+    "ID": 13,
+    "name": "Zapalka",
+    "element": "fire",
+    "value": 1,
+    "image": "im_fire_1"
+};
+
+let fire_12 =
+{
+    "ID": 24,
+    "name": "Bomba atomowa",
+    "flavour": "Calkiem jeszcze nowa",
+    "element": "fire",
+    "value": 12,
+    "image": "im_fire_12"
+};
+
+let water_1 =
+{
+    "ID": 26,
+    "name": "Rosa",
+    "element": "water",
+    "value": 1,
+    "image": "im_water_1"
 };
