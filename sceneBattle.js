239b883fd966a650a5ed0b1e2b398de40168c575
Mac15001900 <mac15001900@gmail.com>
2021-08-18 18:33:47 +0200

    Poprawki, żeby nowy kod działał ze zmianami w Hand

--- sceneBattle.js
+++ sceneBattle.js
@@ -1,21 +1,19 @@
 const PHASE =
-{
-    REST: 0,
-    MOVE: 1
-};
+    {
+        REST: 0,
+        MOVE: 1
+    };
 
 let SceneBattle = new Phaser.Class({
 
     Extends: Phaser.Scene,
 
     initialize:
-        function SceneBattle()
-        {
-            Phaser.Scene.call(this, { key: 'SceneBattle' });
-        },
+    function SceneBattle() {
+        Phaser.Scene.call(this, { key: 'SceneBattle' });
+    },
 
-    preload: function ()
-    {
+    preload: function () {
         console.log('Preload in battle scene');
         //Bazowe karty. Muszą być rozmiaru 1300x800 (przynajmniej dopóki nie napiszemy Card ładniej)
         this.load.image('card_fire', 'assets/card_fire.png');
@@ -53,8 +51,7 @@ let SceneBattle = new Phaser.Class({
         this.load.image('im_only_values', 'assets/only_values.png');
     },
 
-    create: function ()
-    {
+    create: function () {
         /*this.fire_1 = new Card(this, fire_1, 200, 200, 0.2);
         this.forest_1 = new Card(this, forest_1, 400, 200, 0.2);
         this.water_1 = new Card(this, water_1, 600, 200, 0.2);
@@ -77,13 +74,11 @@ let SceneBattle = new Phaser.Class({
         this.hand2 = new Hand(this, 5, this.deck2, 300);*/
         this.testBattle = new Battle(this);
     },
-    update: function (timestep, dt)
-    {
+    update: function (timestep, dt) {
 
     },
 
-    receiveMessage: function (data)
-    {
+    receiveMessage: function (data) {
         console.log(data);
     },
 
@@ -92,87 +87,81 @@ let SceneBattle = new Phaser.Class({
 let Card = new Phaser.Class({
 
     initialize:
-        function Card(scene, data, x, y, scale, hand)
-        {
-            this.scale = scale;
-            this.hand = hand;
-            this.data = data; //Informacje o karcie (żywioł, wartość itp.)
-            this.visual = scene.add.container(x, y); //Wizualne elementy karty
+    function Card(scene, data, x, y, scale, hand) {
+        this.scale = scale;
+        this.hand = hand;
+        this.data = data; //Informacje o karcie (żywioł, wartość itp.)
+        this.visual = scene.add.container(x, y); //Wizualne elementy karty
 
-            switch (data.element)
-            {
-                case ELEMENT.FIRE: this.sprite = scene.add.image(0, 0, 'card_fire').setScale(scale); break;
-                case ELEMENT.FOREST: this.sprite = scene.add.image(0, 0, 'card_forest').setScale(scale); break;
-                case ELEMENT.WATER: this.sprite = scene.add.image(0, 0, 'card_water').setScale(scale); break;
+        switch (data.element) {
+            case ELEMENT.FIRE: this.sprite = scene.add.image(0, 0, 'card_fire').setScale(scale); break;
+            case ELEMENT.FOREST: this.sprite = scene.add.image(0, 0, 'card_forest').setScale(scale); break;
+            case ELEMENT.WATER: this.sprite = scene.add.image(0, 0, 'card_water').setScale(scale); break;
+        }
+
+        this.outline = scene.add.rectangle(0, 0, this.sprite.width * scale + 4, this.sprite.height * scale + 4, 0xffffff);
+        this.outline.setDepth(-5);
+        this.outline.setVisible(false);
+
+        this.image = scene.add.image(0, - 200 * scale, data.image).setScale(scale);
+        //this.image.setScale(64 / this.image.height); //Skalujemy obrazek, żeby jego wysokość wynosiła 64
+
+        //this.nameTextfont = (100 * scale).toString() + "px Arial";
+        this.nameTextfont = ((100 * 12 / (Math.max(data.name.length - 12, 0) + 12)) * scale).toString() + "px Arial";
+        this.nameText = scene.add.text(0, 0, data.name, { font: this.nameTextfont, fill: "#000000" });
+        this.nameText.setOrigin(0.5, 0.5);
+        //this.nameText.x = x;
+        this.nameText.y = - 525 * scale;
+
+        this.valueTextfont = "bold " + (140 * scale).toString() + "px Arial";
+        this.valueText = scene.add.text(0, 0, data.value, { font: this.valueTextfont, fill: "#000000" });
+        this.valueText.setOrigin(0.5, 0.5);
+        //this.valueText.x = x;
+        this.valueText.y = 585 * scale;
+
+        this.effectTextfont = (60 * scale).toString() + "px Arial";
+        this.effectText = scene.add.text(0, 0, Get_effect_string(data.effect), { font: this.effectTextfont, fill: "#000000", wordWrap: { width: 700 * scale }, align: 'left' });
+        this.effectText.setOrigin(0, 0);
+        this.effectText.x = - 325 * scale;
+        this.effectText.y = + 75 * scale;
+
+        this.flavourTextfont = "italic " + (60 * scale).toString() + "px Arial";
+        this.flavourText = scene.add.text(0, 0, data.flavour, { font: this.flavourTextfont, fill: "#000000", wordWrap: { width: 700 * scale }, align: 'left' });
+        this.flavourText.setOrigin(0, 0);
+        this.flavourText.x = - 325 * scale;
+        this.flavourText.y = + 400 * scale;
+
+        this.visual.add([this.outline, this.sprite, this.image, this.nameText, this.valueText, this.effectText, this.flavourText]);
+
+        this.sprite.setInteractive().on('pointerup', () => {
+            switch (this.hand.phase) {
+                case PHASE.MOVE:
+                    console.log("Kilknieto " + this.nameText.text);
+                    this.hand.phase = PHASE.REST;
+                    scene.testBattle.Add_card(this);
+                    this.hand.removeCard(this);
+                    this.hand.drawCard();
+                    break;
+                case PHASE.REST:
+                    console.log("Juz wybrales swoja karte.");
+                    break;
             }
+        });
 
-            this.outline = scene.add.rectangle(0, 0, this.sprite.width * scale + 4, this.sprite.height * scale + 4, 0xffffff);
-            this.outline.setDepth(-5);
+        this.sprite.on('pointerover', () => {
+            this.visual.setScale(1.05);
+            this.visual.y -= 10;
+            this.outline.setVisible(true);
+            //TODO Zmiana 'depth', przy ręce z wieloma kartami
+        });
+        this.sprite.on('pointerout', () => {
+            this.visual.setScale(1);
+            this.visual.y += 10;
             this.outline.setVisible(false);
+        })
 
-            this.image = scene.add.image(0, - 200 * scale, data.image).setScale(scale);
-            //this.image.setScale(64 / this.image.height); //Skalujemy obrazek, żeby jego wysokość wynosiła 64
-
-            //this.nameTextfont = (100 * scale).toString() + "px Arial";
-            this.nameTextfont = ((100 * 12 / (Math.max(data.name.length - 12, 0) + 12)) * scale).toString() + "px Arial";
-            this.nameText = scene.add.text(0, 0, data.name, { font: this.nameTextfont, fill: "#000000" });
-            this.nameText.setOrigin(0.5, 0.5);
-            //this.nameText.x = x;
-            this.nameText.y = - 525 * scale;
-
-            this.valueTextfont = "bold " + (140 * scale).toString() + "px Arial";
-            this.valueText = scene.add.text(0, 0, data.value, { font: this.valueTextfont, fill: "#000000" });
-            this.valueText.setOrigin(0.5, 0.5);
-            //this.valueText.x = x;
-            this.valueText.y = 585 * scale;
-
-            this.effectTextfont = (60 * scale).toString() + "px Arial";
-            this.effectText = scene.add.text(0, 0, Get_effect_string(data.effect), { font: this.effectTextfont, fill: "#000000", wordWrap: { width: 700 * scale }, align: 'left' });
-            this.effectText.setOrigin(0, 0);
-            this.effectText.x = - 325 * scale;
-            this.effectText.y = + 75 * scale;
-
-            this.flavourTextfont = "italic " + (60 * scale).toString() + "px Arial";
-            this.flavourText = scene.add.text(0, 0, data.flavour, { font: this.flavourTextfont, fill: "#000000", wordWrap: { width: 700 * scale }, align: 'left' });
-            this.flavourText.setOrigin(0, 0);
-            this.flavourText.x = - 325 * scale;
-            this.flavourText.y = + 400 * scale;
-
-            this.visual.add([this.outline, this.sprite, this.image, this.nameText, this.valueText, this.effectText, this.flavourText]);
-
-            this.sprite.setInteractive().on('pointerup', () =>
-            {
-                switch (this.hand.phase)
-                {
-                    case PHASE.MOVE:
-                        console.log("Kilknieto " + this.nameText.text);
-                        this.hand.phase = PHASE.REST;
-                        scene.testBattle.Add_card(this);
-                        this.hand.removeCard(scene, this);
-                        this.hand.drawCard(scene);
-                        break;
-                    case PHASE.REST:
-                        console.log("Juz wybrales swoja karte.");
-                        break;
-                }
-            });
-
-            this.sprite.on('pointerover', () =>
-            {
-                this.visual.setScale(1.05);
-                this.visual.y -= 10;
-                this.outline.setVisible(true);
-                //TODO Zmiana 'depth', przy ręce z wieloma kartami
-            });
-            this.sprite.on('pointerout', () =>
-            {
-                this.visual.setScale(1);
-                this.visual.y += 10;
-                this.outline.setVisible(false);
-            })
-
-            // this.name
-        },
+        // this.name
+    },
 
 });
 
@@ -192,6 +181,7 @@ let Hand = new Phaser.Class({
         this.size = size;
         this.cards = [];
         this.deck = deck;
+        this.scene = scene;
         this.phase = PHASE.MOVE;
         this.cardY = scene.sys.game.canvas.height - this.params.cardBaseHeight * this.params.cardScale / 2 - this.params.bottomPadding;
         console.assert(size > 0);
@@ -200,13 +190,13 @@ let Hand = new Phaser.Class({
     },
 
     drawCard: function () {
-        this.cards.push(new Card(this.scene, this.deck.pop(), 0, this.cardY, this.params.cardScale));
+        this.cards.push(new Card(this.scene, this.deck.pop(), 0, this.cardY, this.params.cardScale, this));
         this.repositionCards(this.scene);
     },
 
     drawUntilLimit: function () {
         while (this.cards.length < this.size) {
-            this.drawCard(scene);
+            this.drawCard();
         }
     },
 
@@ -227,11 +217,9 @@ let Hand = new Phaser.Class({
             for (let i = 0; i < this.cards.length; i++) {
                 this.cards[i].visual.x = screenWidth / 2 - fullWidth / 2 + i * (cardWidth + padding) + cardWidth / 2;
             }
-        } else
-        {
+        } else {
             console.warn("Ręka nie miejści się na ekranie");
-            for (let i = 0; i < this.cards.length; i++)
-            {
+            for (let i = 0; i < this.cards.length; i++) {
                 this.cards[i].visual.x = cardWidth / 2 + i * (screenWidth - cardWidth) / (this.cards.length - 1);
             }
         }
@@ -243,27 +231,23 @@ let Hand = new Phaser.Class({
 let Battle = new Phaser.Class({
 
     initialize:
-        function Battle(scene)
-        {
-            this.cards = [];
-            this.effects = [];
-            this.playerDeck = [forest_1, forest_2, forest_3, forest_4, forest_5, forest_6, plus5_forest, fire_1, fire_2, fire_3, fire_4, fire_5, fire_6, minus5_fire, water_1, water_2, water_3, water_4, water_5, water_6, water_7];
-            this.enemyDeck = [forest_1, forest_2, forest_3, forest_4, forest_5, forest_6, plus5_forest, fire_1, fire_2, fire_3, fire_4, fire_5, fire_6, minus5_fire, water_1, water_2, water_3, water_4, water_5, water_6, water_7];
-            Phaser.Actions.Shuffle(this.playerDeck);
-            Phaser.Actions.Shuffle(this.enemyDeck);
-            this.playerHand = new Hand(scene, 5, this.playerDeck, 600);
-            this.enemyHand = new Hand(scene, 5, this.enemyDeck, 300);
-        },
+    function Battle(scene) {
+        this.cards = [];
+        this.effects = [];
+        this.playerDeck = [forest_1, forest_2, forest_3, forest_4, forest_5, forest_6, plus5_forest, fire_1, fire_2, fire_3, fire_4, fire_5, fire_6, minus5_fire, water_1, water_2, water_3, water_4, water_5, water_6, water_7];
+        this.enemyDeck = [forest_1, forest_2, forest_3, forest_4, forest_5, forest_6, plus5_forest, fire_1, fire_2, fire_3, fire_4, fire_5, fire_6, minus5_fire, water_1, water_2, water_3, water_4, water_5, water_6, water_7];
+        Phaser.Actions.Shuffle(this.playerDeck);
+        Phaser.Actions.Shuffle(this.enemyDeck);
+        this.playerHand = new Hand(scene, 5, this.playerDeck, 600);
+        this.enemyHand = new Hand(scene, 5, this.enemyDeck, 300);
+    },
 
-    Add_card: function (new_card)
-    {
+    Add_card: function (new_card) {
         this.cards.push(new_card);
-        if (this.cards.length >= 2)
-        {
+        if (this.cards.length >= 2) {
             var score = Check_who_wins(this.cards[0].data, this.cards[1].data, this.effects);
             this.effects = Update_current_effects(this.cards[0].data, this.cards[1].data, score, this.effects);
-            switch (score)
-            {
+            switch (score) {
                 case 1: console.log(this.cards[0].data.name + " wins!"); break;
                 case -1: console.log(this.cards[1].data.name + " wins!"); break;
                 case 0: console.log("It's a draw"); break;
