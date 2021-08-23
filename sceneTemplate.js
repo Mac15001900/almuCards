0301eda600e2fc217b072a28c21d118a32e3c00b
Mac15001900 <mac15001900@gmail.com>
2021-08-24 01:32:19 +0200

    Lepiej opisany template na sceny

--- sceneTemplate.js
+++ sceneTemplate.js
@@ -1,26 +1,33 @@
 let SceneTemplate = new Phaser.Class({
-
+    //TODO: Dodaj ten plik do index.html
+    //TODO: Dodaj nazwę sceny w main.js, w definicji 'game'
     Extends: Phaser.Scene,
 
     initialize:
     function SceneTemplate() {
-        Phaser.Scene.call(this, { key: 'SceneTemplate' });
+        Phaser.Scene.call(this, { key: 'SceneTemplate' }); //Tu zazwyczaj nic więcej nie trzeba
+    },
+
+    init: function (data) {
+        //Inicjalizacja wstępna, głównie do zapisania argumentów, z jakimi ta scena została wywołana, dostępnych w 'data'.
+        //Jeśli nie używasz 'data' możesz tą funkcję spokojnie usunąć
     },
 
     preload: function () {
         console.log('Preload in template scene');
+        //Miejsce na ładowanie obrazków
     },
 
     create: function () {
-
+        //Miejsce na tworzenie elementów sceny. Wywoływane, kiedy wszystkie obrazki się załadują
     },
 
     update: function (timestep, dt) {
-
+        //Funkcja wywoływana w każdej klatce
     },
 
     receiveMessage: function (data) {
-        console.log(data);
+        //Funkcja wywoływana, gdy otrzymamy nową wiadomość ze ScaleDrone'a
     },
 
 });
