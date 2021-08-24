aa88c2e144e11a96706a9d4b5e559ab9f84de67e
Mac15001900 <mac15001900@gmail.com>
2021-08-24 18:57:19 +0200

    Ulepszony i przetłumaczony debugConfig

--- debugConfig.js
+++ debugConfig.js
@@ -1,10 +1,12 @@
 var debugConfig = {
-	"allow_debug": true, //Setting this to false will disable all other debug options
-	"dev_server": true, //Skips room prompt, uses sever 'dev'
-	"random_server": false, //Skips room promt, uses a server with a random name
-	"random_username": true, //Skip username prompt, uses a random username
-	"disable_messages": false, //Disables sending network messages
-	"log_messages": true, //Logs all received messages in console
+	"allow_debug": false, //Ustawienie tego na 'false' wyłączy wszystkie inne opcje debugowe
+	"dev_server": false, //Zamiast pytać o nazwę serwera łączy od razu z 'dev'.
+	"random_server": false, //Zamiast pytać o nazwę serwera wybiera losową
+	"custom_server": true, //Zamiast pytać o nazwę serwera używa tej ustawionej w 'custom_server_name'
+	"custom_server_name": "Change me", //Nazwa serwera do powyższej opcji.
+	"random_username": true, //Zamiast pytać o nazwę użytkownika używa losowej
+	"disable_messages": false, //Wyłącza wszystkie wiadomości sieciowe
+	"log_messages": true, //Wypisuje wszystkie wiadomości sieciowe w konsoli
 }
 
 //Handle complete debug disabling
