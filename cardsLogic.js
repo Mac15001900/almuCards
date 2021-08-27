fdbe9b320f50ba426855a8686c76079d968449ea
Rad1618 <81822443+Rad1618@users.noreply.github.com>
2021-08-27 12:26:18 +0200

    Wielki refaktoring

---
+++ cardsLogic.js
@@ -0,0 +1,23 @@
+let cardsLogic = {
+    getWinner: function (cardPlayer, cardEnemy, currentEffects)
+    {
+        effectsTable = EffectBank.getInturnEffectsTable(cardPlayer, cardEnemy, currentEffects);    //translate effects to simple table
+        //elements check
+        if (!effectsTable[7])
+        {
+            if (cardPlayer.element === ELEMENT.WATER && cardEnemy.element === ELEMENT.FIRE || cardPlayer.element === ELEMENT.FIRE && cardEnemy.element === ELEMENT.FOREST || cardPlayer.element === ELEMENT.FOREST && cardEnemy.element === ELEMENT.WATER)
+                return 1 * effectsTable[4];
+            if (cardPlayer.element === ELEMENT.WATER && cardEnemy.element === ELEMENT.FOREST || cardPlayer.element === ELEMENT.FIRE && cardEnemy.element === ELEMENT.WATER || cardPlayer.element === ELEMENT.FOREST && cardEnemy.element === ELEMENT.FIRE)
+                return -1 * effectsTable[4];
+        }
+        //value check
+        if (!effectsTable[8])
+        {
+            if ((cardPlayer.value + effectsTable[0]) * effectsTable[2] > (cardEnemy.value + effectsTable[1]) * effectsTable[3])
+                return 1 * effectsTable[5];
+            if ((cardPlayer.value + effectsTable[0]) * effectsTable[2] < (cardEnemy.value + effectsTable[1]) * effectsTable[3])
+                return -1 * effectsTable[5];
+        }
+        return 0;
+    },
+};
